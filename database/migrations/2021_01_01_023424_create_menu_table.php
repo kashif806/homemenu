f3a167e47ecc08b7f49fb197f596ac33205fc55d
Kashif Hussain <kashif806@gmail.com>
2021-01-01 21:58:40 +1100

    cont. migrations anf frontend react app

---
+++ database/migrations/2021_01_01_023424_create_menu_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('dishes_id');
+            $table->string('type');
+            $table->string('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+}
